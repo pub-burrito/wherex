231a9f9ecb9a8ea082662bcd184144ace61927f6
Philip Wiebe <phil@cloud.com>
2013-11-22 11:09:05 -0800

    upgrade to rails 4

--- lib/wherex/visitor.rb
+++ lib/wherex/visitor.rb
@@ -8,21 +8,21 @@ module Wherex
       end
     end
 
-    def visit_Arel_Nodes_Equality_with_wherex o
+    def visit_Arel_Nodes_Equality_with_wherex o, attribute = nil
       right = o.right
       if right.present? and right.is_a? Regexp
         ::ActiveRecord::Base.connection.regexp visit(o.left), visit(right)
       else
-        visit_Arel_Nodes_Equality_without_wherex o
+        visit_Arel_Nodes_Equality_without_wherex o, attribute
       end
     end
 
-    def visit_Arel_Nodes_NotEqual_with_wherex o
+    def visit_Arel_Nodes_NotEqual_with_wherex o, attribute = nil
       right = o.right
       if right.present? and right.is_a? Regexp
         ::ActiveRecord::Base.connection.regexp_not visit(o.left), visit(right)
       else
-        visit_Arel_Nodes_NotEqual_without_wherex o
+        visit_Arel_Nodes_NotEqual_without_wherex o, attribute
       end
     end
 
